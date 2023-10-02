fbc4993c38b908752ef8f7ffa6a46bb734bf3a46
Progamit <140381653+Progamit@users.noreply.github.com>
2023-10-02 12:05:08 +0300

    Update GameMethods.js

--- modules/GameMethods.js
+++ modules/GameMethods.js
@@ -1,25 +1,25 @@
-const rnd = (num) => Math.round(Math.random() * num)
+// const rnd = (num) => Math.round(Math.random() * num)
 
 
-module.exports = {
+// module.exports = {
 
-    spin: (bidAmount) => {
-        const possibleNums = [20, 50, 100, 200, 500, 1000]
-        const result = [
-            rnd(5),
-            rnd(5),
-            rnd(5)
-        ]
-        let moneyWon = 0
+//     spin: (bidAmount) => {
+//         const possibleNums = [20, 50, 100, 200, 500, 1000]
+//         const result = [
+//             rnd(5),
+//             rnd(5),
+//             rnd(5)
+//         ]
+//         let moneyWon = 0
 
-        possibleNums.map((x, i) => {
-            if (result[0] === i && result[0] === result[1] && result[0] === result[2]) moneyWon = bidAmount * x
-        })
+//         possibleNums.map((x, i) => {
+//             if (result[0] === i && result[0] === result[1] && result[0] === result[2]) moneyWon = bidAmount * x
+//         })
 
-        return {
-            moneyWon,
-            result
-        }
-    }
+//         return {
+//             moneyWon,
+//             result
+//         }
+//     }
 
-}
+// }
