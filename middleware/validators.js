0533161d0e7a742dc28c4b3699b46b2bcc26c7ac
Progamit <140381653+Progamit@users.noreply.github.com>
2023-10-02 12:04:26 +0300

    Update validators.js

--- middleware/validators.js
+++ middleware/validators.js
@@ -1,30 +1,30 @@
-const jwt = require("jsonwebtoken")
+// const jwt = require("jsonwebtoken")
 
-module.exports = {
-    validateUser: (req, res, next) => {
-        const {
-            username,
-            pass1,
-            pass2
-        } = req.body
+// module.exports = {
+//     validateUser: (req, res, next) => {
+//         const {
+//             username,
+//             pass1,
+//             pass2
+//         } = req.body
 
-        if(username.length === 0) return res.send({error: true, data: null, message: "Bad username"})
-        if(pass1.length === 0) return res.send({error: true, data: null, message: "Bad password lenght"})
-        if(pass1 !== pass2) return res.send({error: true, data: null, message: "passwords should match"})
+//         if(username.length === 0) return res.send({error: true, data: null, message: "Bad username"})
+//         if(pass1.length === 0) return res.send({error: true, data: null, message: "Bad password lenght"})
+//         if(pass1 !== pass2) return res.send({error: true, data: null, message: "passwords should match"})
 
-        next()
-    },
-    authorize: (req, res, next) => {
-        const token = req.headers.authorization
+//         next()
+//     },
+//     authorize: (req, res, next) => {
+//         const token = req.headers.authorization
 
 
 
-        jwt.verify(token, process.env.JWT_SECRET, async (err, item) => {
-            if(err) {
-                return res.send({error: true, message: "bad auth"})
-            }
-            req.user = item
-            return next()
-        })
-    }
-}
+//         jwt.verify(token, process.env.JWT_SECRET, async (err, item) => {
+//             if(err) {
+//                 return res.send({error: true, message: "bad auth"})
+//             }
+//             req.user = item
+//             return next()
+//         })
+//     }
+// }
